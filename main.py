f17d03330c9f79e4977dee67cc83c2d44b3c3825
David Ward <davidmward@mail.com>
2023-11-11 14:21:26 -0800

    open file and do error checking

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ def Raytrace(r):
     return 0
 
 def main():
+
+    # open file
+    filePath = "text.txt"
+    try:
+        with open(filePath, 'r') as file:
+            data = file.read()
+            print(data)
+    except FileNotFoundError:
+        print(f"The file at '{filePath}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    
+
+    # do raytracing
     pixelList = []
     for pixel in pixelList:
         color = Raytrace()

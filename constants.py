124166376906a469595096bc4050705047f39387
David Ward <davidmward@mail.com>
2023-11-12 14:44:24 -0800

    store constant strings in object

---
+++ constants.py
@@ -0,0 +1,13 @@
+class Constants
+    def __init__(self):
+        self.sphere = "SPHERE"
+        self.light = "LIGHT"
+        self.near = "NEAR"
+        self.left = "LEFT"
+        self.right = "RIGHT"
+        self.bottom = "BOTTOM"
+        self.top = "TOP"
+        self.res = "RES"
+        self.ambient = "AMBIENT"
+        self.back = "BACK"
+        self.output = "OUTPUT"

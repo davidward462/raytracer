27e133c53fca376f61630c18e9e0bfcba8821aab
David Ward <davidmward@mail.com>
2023-11-12 13:58:13 -0800

    parse sphere attribute string

--- sphere.py
+++ sphere.py
@@ -1,7 +1,10 @@
 class Sphere:
-    def __init__(self, name, attributeList):
-        self.name = name
-        self.attributeList = attributeList
+    def __init__(self, attributeString):
+        self.attributeString = attributeString
+        self.attributeList = []
+    
+        # attributes for calculations
+        self.name = ""
         self.posX = 0
         self.posY = 0
         self.posZ = 0
@@ -17,7 +20,31 @@ class Sphere:
         self.kr = 0
         self.n = 0
 
+        self.attributeCount = 15
+        
     def Parse(self):
+        self.attributeList = self.attributeString.split(' ')
+        inputAttributeCount = len(self.attributeList)
+
+        if (inputAttributeCount != self.attributeCount):
+            print(" Incorrect number of attributes in sphere input.")
+        else:
+            # correct number of attributes
+            self.name = self.attributeList[0] 
+            self.posX = self.attributeList[1]
+            self.posY = self.attributeList[2]
+            self.posZ = self.attributeList[3]
+            self.scaleX = self.attributeList[4]
+            self.scaleY = self.attributeList[5]
+            self.scaleZ = self.attributeList[6]
+            self.r = self.attributeList[7]
+            self.g = self.attributeList[8]
+            self.b = self.attributeList[9]
+            self.ka = self.attributeList[10]
+            self.kd = self.attributeList[11]
+            self.ks = self.attributeList[12]
+            self.kr = self.attributeList[13]
+            self.n = self.attributeList[14]
         return 0
 
     def PrintShort(self):

2c585f70283278c51d6b88523c0e351961941f6a
David Ward <davidmward@mail.com>
2023-11-12 14:00:48 -0800

    parse light string input

--- light.py
+++ light.py
@@ -1,12 +1,35 @@
 class Light:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, attributeString):
+        self.attributeString = attributeString
+        self.attributeList = []
+    
+        # attributes for calculations
+        self.name = ""
         self.posX = 0
         self.posY = 0
         self.posZ = 0
         self.ir = 0
         self.ig = 0
         self.ib = 0
+
+        self.attributeCount = 7
+        
+    def Parse(self):
+        self.attributeList = self.attributeString.split(' ')
+        inputAttributeCount = len(self.attributeList)
+
+        if (inputAttributeCount != self.attributeCount):
+            print(" Incorrect number of attributes in light input.")
+        else:
+            # correct number of attributes
+            self.name = self.attributeList[0] 
+            self.posX = self.attributeList[1]
+            self.posY = self.attributeList[2]
+            self.posZ = self.attributeList[3]
+            self.ir = self.attributeList[4]
+            self.ig = self.attributeList[5]
+            self.ib = self.attributeList[6]
+        return 0
 
     def PrintShort(self):
         return f"\nname: {self.name}\nx: {self.posX}\ny: {self.posY}\nz: {self.posZ}"

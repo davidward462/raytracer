47f396718c62863d88a9c249f46a2fa2222e86d3
David Ward <davidmward@mail.com>
2023-11-12 11:46:53 -0800

    move print()

--- main.py
+++ main.py
@@ -21,15 +21,20 @@ def main():
 
     # open file
     filePath = fileArgs[1]
+    data = ""
     try:
         with open(filePath, 'r') as file:
             data = file.read()
-            print(data)
+
     # catch errors
     except FileNotFoundError:
         print(f" The file at '{filePath}' was not found.")
     except Exception as e:
         print(f" An error occurred: {e}")
+
+
+    # process data
+    print(data)
 
 
     # do raytracing

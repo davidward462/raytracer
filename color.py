eec0ad95596e35034d5e496e4f589e93b449df28
David Ward <davidmward@mail.com>
2023-11-19 12:41:17 -0800

    set and get color value in class

--- color.py
+++ color.py
@@ -4,6 +4,12 @@ class Color:
         self.g = g
         self.b = b
         self.value = (r, g, b)
+
+    def SetValue(self, value):
+        self.value = value
+
+    def GetValue(self):
+        return self.value
         
     def __str__(self):
         return f" rgb({self.value})"

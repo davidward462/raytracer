5d06b5df3b5cb1cb334e60c38f0873e8e696787a
David Ward <davidmward@mail.com>
2023-11-20 18:07:19 -0800

    simplify color.py to use numpy array

--- color.py
+++ color.py
@@ -3,30 +3,23 @@ import numpy as np
 class Color:
     def __init__(self, r=0, g=0, b=0):
         self.rgb = np.array([r, g, b])
-        self.r = r
-        self.g = g
-        self.b = b
-        self.value = (self.r, self.g, self.b)
-
-    def SetValue(self, value):
-        self.value = value
-
-    def GetValue(self):
-        return self.value
 
     def SetRGB(self, r ,g ,b):
-        self.r = r
-        self.g = g
-        self.b = b
         self.rgb[0] = r
         self.rgb[1] = g
         self.rgb[2] = b
 
+    def Red(self):
+        return self.rgb[0]
+
+    def Green(self):
+        return self.rgb[1]
+
+    def Blue(self):
+        return self.rgb[2]
+
     def AddColor(self, inColor):
-        sumR = self.r + inColor.r
-        sumG = self.g + inColor.g
-        sumB = self.b + inColor.b
-        self.SetRGB(sumR, sumG, sumB)
+        self.SetRGB(inColor.rgb[0], inColor.rgb[1], inColor.rgb[2])
         
     def __str__(self):
-        return f"rgb({self.r}, {self.g}, {self.b})"
+        return f"rgb({self.rgb[0]}, {self.rgb[1]}, {self.rgb[2]})"

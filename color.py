b17024adaaa72fc6d5c2adf9336cd8eac0abcd7e
David Ward <davidmward@mail.com>
2023-11-20 17:09:10 -0800

    update color class

--- color.py
+++ color.py
@@ -1,15 +1,20 @@
 class Color:
-    def __init__(self, r, g, b):
+    def __init__(self, r=0, g=0, b=0):
         self.r = r
         self.g = g
         self.b = b
-        self.value = (r, g, b)
+        self.value = (self.r, self.g, self.b)
 
     def SetValue(self, value):
         self.value = value
 
     def GetValue(self):
         return self.value
+
+    def SetRGB(self, r ,g ,b):
+        self.r = r
+        self.g = g
+        self.b = b
         
     def __str__(self):
-        return f" rgb({self.value})"
+        return f" rgb({self.r}, {self.g}, {self.b})"

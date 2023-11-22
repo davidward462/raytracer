8de5ea303731c0057f9ca3d610c758793ac36221
David Ward <davidmward@mail.com>
2023-11-21 23:15:51 -0800

    add direction to ray

--- ray.py
+++ ray.py
@@ -1,6 +1,7 @@
 class Ray:
-    def __init__(self, origin):
+    def __init__(self, origin, direction):
         self.origin = origin
+        self.direction = direction
         self.depth = 0
 
     def SetDepth(self, depth):
@@ -9,5 +10,11 @@ class Ray:
     def GetDepth(self):
         return self.depth
 
+    def GetOrigin(self):
+        return self.origin
+
+    def GetDirection(self):
+        return self.direction
+
     def __str__(self):
-        return f" origin: {self.origin}\ndepth: {self.depth}"
+        return f" origin: {self.origin}\ndepth: {self.depth}\ndirection: {self.direction}"

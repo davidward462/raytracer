c8792a01078a7e1e7d828348cac616d8017ea8b2
David Ward <davidmward@mail.com>
2023-11-21 21:32:48 -0800

    rename variable in color class

--- color.py
+++ color.py
@@ -2,24 +2,24 @@ import numpy as np
 
 class Color:
     def __init__(self, r=0, g=0, b=0):
-        self.rgb = np.array([r, g, b])
+        self.value = np.array([r, g, b])
 
     def SetRGB(self, r ,g ,b):
-        self.rgb[0] = r
-        self.rgb[1] = g
-        self.rgb[2] = b
+        self.value[0] = r
+        self.value[1] = g
+        self.value[2] = b
 
     def Red(self):
-        return self.rgb[0]
+        return self.value[0]
 
     def Green(self):
-        return self.rgb[1]
+        return self.value[1]
 
     def Blue(self):
-        return self.rgb[2]
+        return self.value[2]
 
     def AddColor(self, inColor):
-        self.rgb = self.rgb + inColor.rgb
+        self.value = self.value + inColor.value
         
     def __str__(self):
-        return f"rgb({self.rgb[0]}, {self.rgb[1]}, {self.rgb[2]})"
+        return f"rgb({self.value[0]}, {self.value[1]}, {self.value[2]})"
--- test.py
+++ test.py
@@ -20,6 +20,13 @@ def main():
     c1 = color.Color(0.5, 0.6, 0.7)
     c2 = color.Color(0.1, 0.2, 0.3)
     testColorAdd(c1, c2)
+    
+    x = color.Color(0, 0.1, 0.2)
+    y = color.Color(0.2, 0.3, 0.4)
+    z = color.Color(0.1, 0.1, 0.1)
+    result = x.value + y.value + z.value
+    print(result)
+
 
     return 0
 
